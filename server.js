a4311483cae075d057122b934645085e4f1b15fb
Kradek <kradekmfc@gmail.com>
2013-08-14 19:16:11 -0400

    error on user does not exist

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.get('/', function(req, res, next) {
         socket.listen("message", function(m){
             if (!(m.Type == MFCMessageType.FCTYPE_USERNAMELOOKUP))
                 return;
+            if (m.Arg2 == MFCResponseType.FCRESPONSE_ERROR)
+                res.json({error: "User does not exist or MFC error."})
 
             res.json(m.Data);
         })
@@ -201,6 +203,68 @@ function MFCSocket(name, passCode){
 
 };
 
+function MFCMessage(initializer){
+    var self = this;
+
+    if ("string" === typeof(initializer)){
+        //strip out newlines
+        initializer = initializer.replace(/(\r\n|\n|\r)/gm, "");
+
+        //parse out the typical pieces
+        ["Type","From","To","Arg1","Arg2"].forEach(function(part){
+            var delimiterPos = initializer.indexOf(" ");
+            self[part] = initializer.substring(0, delimiterPos);
+            initializer = initializer.substring(delimiterPos + 1)
+        });
+
+        //convert Type to an int
+        self.Type = parseInt(self.Type,10);
+
+        //parse out data if there is any
+        if (initializer.length > 0){
+
+            if (self.Type != MFCMessageType.FCTYPE_LOGIN) {
+                var jsonPayload = [
+                    MFCMessageType.FCTYPE_DETAILS,
+                    MFCMessageType.FCTYPE_ADDFRIEND,
+                    MFCMessageType.FCTYPE_ADDIGNORE,
+                    MFCMessageType.FCTYPE_SESSIONSTATE,
+                    MFCMessageType.FCTYPE_CMESG,
+                    MFCMessageType.FCTYPE_PMESG,
+                    MFCMessageType.FCTYPE_TXPROFILE,
+                    MFCMessageType.FCTYPE_USERNAMELOOKUP,
+                    MFCMessageType.FCTYPE_MYCAMSTATE,
+                    MFCMessageType.FCTYPE_SETGUESTNAME
+                ];
+
+                if (jsonPayload.indexOf(self.Type) != -1 ||
+                    (self.Type == MFCMessageType.FCTYPE_JOINCHAN && self.Arg2 == MFCMessageType.FCCHAN_PART)) {
+                    var parsed;
+                    try {
+                        parsed = JSON.parse(unescape(initializer));
+                    } catch(err){}
+                    self.Data = parsed;
+                }
+            }
+            else
+                self.Data = initializer;
+        }
+    }
+
+    if ("object" === typeof(initializer)){
+        _.extend(self, initializer);
+    }
+
+    self.asMFCMessage = function asMFCMessage(){
+        var msg = _.sprintf("%s %s %s %s %s", self.Type, self.From, self.To, self.Arg1, self.Arg2);
+        if (undefined !== self.Data){
+            msg += " " + self.Data;
+        }
+        msg += "\n\0";
+        return msg;
+    }
+}
+
 var MFCMessageType = {
     FCTYPE_NULL : 0,
     FCTYPE_LOGIN : 1,
@@ -293,67 +357,8 @@ var MFCVideoState = {
     FCVIDEO_RX_GRP: 93,
     FCVIDEO_UNKNOWN: 127
 };
-
-"use strict";
-
-function MFCMessage(initializer){
-    var self = this;
-
-    if ("string" === typeof(initializer)){
-        //strip out newlines
-        initializer = initializer.replace(/(\r\n|\n|\r)/gm, "");
-
-        //parse out the typical pieces
-        ["Type","From","To","Arg1","Arg2"].forEach(function(part){
-            var delimiterPos = initializer.indexOf(" ");
-            self[part] = initializer.substring(0, delimiterPos);
-            initializer = initializer.substring(delimiterPos + 1)
-        });
-
-        //convert Type to an int
-        self.Type = parseInt(self.Type,10);
-
-        //parse out data if there is any
-        if (initializer.length > 0){
-
-            if (self.Type != MFCMessageType.FCTYPE_LOGIN) {
-                var jsonPayload = [
-                    MFCMessageType.FCTYPE_DETAILS,
-                    MFCMessageType.FCTYPE_ADDFRIEND,
-                    MFCMessageType.FCTYPE_ADDIGNORE,
-                    MFCMessageType.FCTYPE_SESSIONSTATE,
-                    MFCMessageType.FCTYPE_CMESG,
-                    MFCMessageType.FCTYPE_PMESG,
-                    MFCMessageType.FCTYPE_TXPROFILE,
-                    MFCMessageType.FCTYPE_USERNAMELOOKUP,
-                    MFCMessageType.FCTYPE_MYCAMSTATE,
-                    MFCMessageType.FCTYPE_SETGUESTNAME,
-                ];
-
-                if (jsonPayload.indexOf(self.Type) != -1 ||
-                    (self.Type == MFCMessageType.FCTYPE_JOINCHAN && self.Arg2 == MFCMessageType.FCCHAN_PART)) {
-                    var parsed;
-                    try {
-                        parsed = JSON.parse(unescape(initializer));
-                    } catch(err){}
-                    self.Data = parsed;
-                }
-            }
-            else
-                self.Data = initializer;
-        }
-    }
-
-    if ("object" === typeof(initializer)){
-        _.extend(self, initializer);
-    }
-
-    self.asMFCMessage = function asMFCMessage(){
-        var msg = _.sprintf("%s %s %s %s %s", self.Type, self.From, self.To, self.Arg1, self.Arg2);
-        if (undefined !== self.Data){
-            msg += " " + self.Data;
-        }
-        msg += "\n\0";
-        return msg;
-    }
-}
+var MFCResponseType = {
+    FCRESPONSE_SUCCESS: 0,
+    FCRESPONSE_ERROR: 1,
+    FCRESPONSE_NOTICE: 2
+};
