e5bbf50abd49c5a468f644a12abad2e44d7a27e6
Kradek <kradekmfc@gmail.com>
2014-05-26 14:43:48 -0400

    add model list

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var cors = require('cors');
+var request = require('request');
 var MFCSocket = require('MFCSocket');
 var MFCMessageType = require('MFCSocket').MFCMessageType;
 var MFCResponseType = require('MFCSocket').MFCResponseType;
-var UserLookup = require('MFCSocket').UserLookup;
 
 var app = express();
 app.use(cors());
@@ -43,6 +43,53 @@ app.get('/', function(req, res, next) {
 
             //return the data package on a successful response
             res.json(m.Data);
+        })
+    });
+});
+
+app.get('/models', function(req, res, next){
+    var socket = new MFCSocket();
+
+    //listen for socket errors
+    socket.listen("error", function(err){
+        res.json({error: "A socket error occurred.", detail: err});
+    });
+
+    //listen for the socket closing
+    socket.listen("close", function(msg){
+        res.json({error: "Socket closed unexpectedly.", detail: msg});
+    })
+
+    //set up a listener for login completion
+    socket.listen("loggedin", function(e){
+        //listen for a lookup response
+        socket.listen("message", function(m){
+            if (!(m.Type == MFCMessageType.FCTYPE_METRICS))
+                return;
+
+            var servers = [
+                { Name: "xchat11", Type: "hybi00" },
+                { Name: "xchat12", Type: "hybi00" },
+                { Name: "xchat20", Type: "hybi00" },
+                { Name: "xchat7", Type: "rfc6455" },
+                { Name: "xchat8", Type: "rfc6455" },
+                { Name: "xchat9", Type: "rfc6455" },
+                { Name: "xchat10", Type: "rfc6455" }
+            ];
+            //choose a random server
+            var server = servers[Math.floor(Math.random() * servers.length)];
+
+            var url = 'http://www.myfreecams.com/mfc2/php/mobj.php?f=' + m.Data.fileno + '&s=' + server.Name;
+
+            request(url, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    res.json({
+                        message: m,
+                        url: url,
+                        response: body
+                    });
+                }
+            })
         })
     });
 });
